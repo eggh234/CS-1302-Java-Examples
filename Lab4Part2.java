90021fcfde8a86dd2234ae2cc070ad7c93f7fa59
eggh234 <61628697+eggh234@users.noreply.github.com>
2021-06-17 12:29:51 -0400

    Update Lab4Part2.java

--- Lab4Part2.java
+++ Lab4Part2.java
@@ -8,8 +8,14 @@ public class Lab4Part2 {
     public static Scanner input = new Scanner(System.in);//initializes scanner
     public static void main(String[] args) {
         System.out.println("Input a word to count: ");
-        User = input.next();//grabs user input
-        int length = User.length();//gets the length of the line of text
-        System.out.println("The length of '" + User + "' is: " + length);//prints it out
+        String User = input.next();
+        if ( Character.isDigit(User.charAt(0)) )//checks if the first value is a number
+        {
+            System.out.println("String begins with a digit");
+        } else {
+            int length = User.length();//gets the length of the line of text
+            System.out.println("The length of '" + User + "' is: " + length);//prints it out
+        }
     }
 }
+
